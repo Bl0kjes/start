c68e2787e6ba9b36afc77355ea6c5d5e07ede704
Bl0kjes <69487610+Bl0kjes@users.noreply.github.com>
2024-04-15 14:23:02 +0200

    Initial commit

---
+++ cardGame.js
@@ -0,0 +1,90 @@
+var cardHTML = document.getElementById("card");
+var scoreHTML = document.getElementById("score");
+var resultHTML = document.getElementById("result");
+
+var possibleCards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+var previousCard = 0;
+var randomNumber = 0;
+
+var numQuestionsAsked = 0;
+var numCorrect = 0;
+var totalReward = 0; // New variable to track the total reward
+
+function resetCardGame() {
+    previousCard = generateCard();
+    cardHTML.innerText = previousCard;
+
+    resultHTML.innerText = "You were: ";
+    scoreHTML.innerText = "You've got " + numCorrect + "/" + numQuestionsAsked + " correct.";
+}
+
+function submitGuess(highLowGuess) {
+    var newCard = generateCard();
+    var correctGuess = guessCard(highLowGuess, newCard);
+    updateScores(correctGuess);
+    modifyCardGameHTML(correctGuess);
+    setPreviousCard(newCard);
+}
+
+function generateCard() {
+    randomNumber = 0;
+    var randomCard = 0;
+
+    do {
+        randomNumber = Math.floor(Math.random() * possibleCards.length);
+        randomCard = possibleCards[randomNumber];
+    } while (previousCard === randomCard);
+
+    return randomCard;
+}
+
+function guessCard(highLowGuess, newCard) {
+    var correctGuess;
+
+    if (highLowGuess === "Higher") {
+        if (newCard > previousCard) {
+            correctGuess = true;
+        } else {
+            correctGuess = false;
+        }
+    } else if (highLowGuess === "Lower") {
+        if (newCard < previousCard) {
+            correctGuess = true;
+        } else {
+            correctGuess = false;
+        }
+    } else {
+        console.log("No guess provided");
+        correctGuess = false;
+    }
+
+    return correctGuess;
+}
+
+function updateScores(correctGuess) {
+    if (correctGuess) {
+        numCorrect++;
+        // Add the reward when the guess is correct
+        totalReward += 10; // Change the amount as needed
+    }
+
+    numQuestionsAsked++;
+}
+
+function modifyCardGameHTML(correctGuess) {
+    var resultString = "";
+
+    if (correctGuess) {
+        resultString = "Correct";
+    } else resultString = "Incorrect";
+
+    resultHTML.innerText = "You were: " + resultString;
+    scoreHTML.innerText = "You've got " + numCorrect + "/" + numQuestionsAsked + " correct. Total Reward: €" + totalReward;
+}
+
+function setPreviousCard(newCard) {
+    previousCard = newCard;
+    cardHTML.innerText = previousCard;
+}
+
+resetCardGame();
